46ebcb8f5fb9127ffedf8019b254d08ec097e1e2
wuluwug <wulunmlss@gmail.com>
2024-10-19 15:46:59 +0200

    wasd

--- scenes/ODS/ods.gd
+++ scenes/ODS/ods.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-func _on_body_entered(body):
+func _on_body_entered(_body):
 	Globals.item += 1
 	queue_free()
 	print(Globals.item)
--- wulu/pj.gd
+++ wulu/pj.gd
@@ -1,16 +1,8 @@
 extends CharacterBody2D
-
-
 const speed = 300.0
 
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-
-
-
-func _physics_process(delta):
-
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+func _physics_process(_delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	move_and_slide()
 
