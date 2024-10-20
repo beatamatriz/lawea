b6070fa7a4d16f91fe83928c9e8ae7f46c786400
Bea <31886814+beatamatriz@users.noreply.github.com>
2024-10-20 11:26:03 +0200

    player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,15 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 700
 
 func _physics_process(delta):
-	
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if direction:
-		velocity = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+	velocity = direction * SPEED
 	move_and_slide()
