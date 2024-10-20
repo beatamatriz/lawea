971c7218cb8188fa529aa5c6f38c59f0a959dcba
Beatriz <31886814+beatamatriz@users.noreply.github.com>
2024-10-20 11:16:15 +0200

    delete wulu

--- wulu/pj.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-const speed = 300.0
-
-func _physics_process(_delta):
-	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * speed
-	move_and_slide()
-
