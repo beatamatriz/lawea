e1a7aca632c53a30e342d74a8e4bddaf47aff676
wuluwug <wulunmlss@gmail.com>
2024-10-20 11:17:03 +0200

    tilemap y salida

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -3,3 +3,9 @@ extends Node2D
 # para cambiar de escena 
 func change_scene(scene_name):
 	get_tree().change_scene_to_file(scene_name)
+
+func _on_salida_body_entered(body):
+	change_scene(#ruta de la escena)
+
+
+
